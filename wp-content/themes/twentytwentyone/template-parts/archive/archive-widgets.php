54a73ddf83c3ab4f16c47d6fa7cde31221ec7edd
Ngoc Phong <ngocphongdnp@gmail.com>
2023-10-25 02:55:46 +0700

    update last

--- wp-content/themes/twentytwentyone/template-parts/archive/archive-widgets.php
+++ wp-content/themes/twentytwentyone/template-parts/archive/archive-widgets.php
@@ -9,7 +9,90 @@
 
 if (is_active_sidebar('archive-1')): ?>
     <div class="container widget-title">
-        <?php dynamic_sidebar('archive-1'); ?>
-    </div>
-    <?php
+        <div class="box-news">
+            <div class="box-news-left">
+                <div class="p-detail-archive">
+                <h2 class="p-title-archive">Xem Nhiều</h2>
+                </div>
+                
+                <div>
+                    <div class="row">
+                        <div class="col-7">
+                            <div class="row">
+                                <div class="col-6 ">
+                                    <div class="box-border">
+                                        <li class="p-month-archive ">
+                                            <span class="p-number">1</span><a href="#">Giáo sư Mỹ tìm lại mẹ ruột cho hai
+                                                con
+                                                gốc Việt</a>
+
+                                        </li>
+                                    </div>
+
+                                    <div class="box-border">
+                                        <li class="p-month-archive ">
+                                            <span class="p-number">2</span><a href="#">Giáo sư Mỹ tìm lại mẹ ruột cho hai
+                                                con
+                                                gốc Việt</a>
+
+                                        </li>
+                                    </div>
+                                    <div class="box-border">
+                                        <li class="p-month-archive ">
+                                            <span class="p-number">3</span><a href="#">Giáo sư Mỹ tìm lại mẹ ruột cho hai
+                                                con
+                                                gốc Việt</a>
+
+                                        </li>
+                                    </div>
+                                    <div class="box-border">
+                                        <li class="p-month-archive ">
+                                            <span class="p-number">4</span><a href="#">Giáo sư Mỹ tìm lại mẹ ruột cho hai
+                                                con
+                                                gốc Việt</a>
+
+                                        </li>
+                                    </div>
+                                </div>
+                                <div class="col-6 box-border box-border-right ">
+                                    <div class="box-border">
+                                        <li class="p-month-archive ">
+                                            <span class="p-number">5</span><a href="#">Giáo sư Mỹ tìm lại mẹ ruột cho hai
+                                                con
+                                                gốc Việt</a>
+
+                                        </li>
+                                    </div>
+                                    <div class="box-border">
+                                        <li class="p-month-archive ">
+                                            <span class="p-number">6</span><a href="#">Giáo sư Mỹ tìm lại mẹ ruột cho hai
+                                                con
+                                                gốc Việt</a>
+
+                                        </li>
+                                    </div>
+                                    <div class="box-border">
+                                        <li class="p-month-archive ">
+                                            <span class="p-number">7</span><a href="#">Giáo sư Mỹ tìm lại mẹ ruột cho hai
+                                                con
+                                                gốc Việt</a>
+                                        </li>
+                                    </div>
+                                    <div class="box-border">
+                                        <li class="p-month-archive ">
+                                            <span class="p-number">8</span><a href="#">Giáo sư Mỹ tìm lại mẹ ruột cho hai
+                                                con
+                                                gốc Việt</a>
+
+                                        </li>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+
+                <?php
 endif;
