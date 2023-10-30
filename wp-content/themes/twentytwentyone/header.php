3dd06871fde3ec5e09315a3d95bde6d9f1a013f5
Hoaan <21211tt3903@mail.tdc.edu.vn>
2023-10-30 16:17:44 +0700

    update css search

--- wp-content/themes/twentytwentyone/header.php
+++ wp-content/themes/twentytwentyone/header.php
@@ -25,6 +25,7 @@
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/style-search.css" type="text/css" media="screen" />
 	<?php wp_head(); ?>
 </head>
 
