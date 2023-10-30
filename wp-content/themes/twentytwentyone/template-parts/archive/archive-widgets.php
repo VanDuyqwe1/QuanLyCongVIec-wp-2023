c30800e060d538081a9a609a5a276c728b6e70c5
Ngoc Phong <ngocphongdnp@gmail.com>
2023-10-31 02:18:01 +0700

    update all

--- wp-content/themes/twentytwentyone/template-parts/archive/archive-widgets.php
+++ wp-content/themes/twentytwentyone/template-parts/archive/archive-widgets.php
@@ -7,54 +7,73 @@
  * @since Twenty Twenty-One 1.0
  */
 
+
+global $wpdb;
+
+// Tên bảng wp_posts
+$table_name = $wpdb->prefix . 'posts';
+
+// Câu truy vấn SQL để lấy dữ liệu từ cột post_name
+$query = "SELECT post_name FROM $table_name";
+
+// Thực thi truy vấn và lấy kết quả
+$results = $wpdb->get_results($query);
+
+// Loop qua kết quả để hiển thị nội dung
+foreach ($results as $result) {
+    $post_name = $result->post_name;
+    echo $post_name . "<br>";
+}
 if (is_active_sidebar('archive-1')): ?>
     <div class="container widget-title">
         <div class="box-news">
             <div class="box-news-left">
-                <div class="p-detail-archive">
-                <h2 class="p-title-archive">Xem Nhiều</h2>
-                </div>
-                
+
                 <div>
                     <div class="row">
-                        <div class="col-7">
+                        <div class="col-12">
                             <div class="row">
-                                <div class="col-6 ">
-                                    <div class="p-box-border">
-                                        <li class="p-month-archive ">
-                                            <span class="p-number">1</span><a href="#">Giáo sư Mỹ tìm lại mẹ ruột cho hai
-                                                con
-                                                gốc Việt</a>
-
-                                        </li>
+                                <div class="wrapper">
+                                    <div class="p-detail-archive">
+                                        <h2 class="p-title-archive">Xem Nhiều</h2>
                                     </div>
-
-                                    <div class="p-box-border">
-                                        <li class="p-month-archive ">
-                                            <span class="p-number">2</span><a href="#">Giáo sư Mỹ tìm lại mẹ ruột cho hai
-                                                con
-                                                gốc Việt</a>
-
-                                        </li>
-                                    </div>
-                                    <div class="p-box-border">
-                                        <li class="p-month-archive ">
-                                            <span class="p-number">3</span><a href="#">Giáo sư Mỹ tìm lại mẹ ruột cho hai
-                                                con
-                                                gốc Việt</a>
-
-                                        </li>
-                                    </div>
-                                    <div class="p-box-border">
-                                        <li class="p-month-archive ">
-                                            <span class="p-number">4</span><a href="#">Giáo sư Mỹ tìm lại mẹ ruột cho hai
-                                                con
-                                                gốc Việt</a>
-
-                                        </li>
+                                    <div class="box">
+                                        <div class="p-box-archive"><span class="number-top-view">2</span>
+                                            <h3 class="p-title">Tướng Trung Quốc: Một số nước cố tình làm thế giới hỗn loạn
+                                            </h3>
+                                        </div>
+                                        <div class="p-box-archive"><span class="number-top-view">2</span>
+                                            <h3 class="p-title">Tướng Trung Quốc: Một số nước cố tình làm thế giới hỗn loạn
+                                            </h3>
+                                        </div>
+                                        <div class="p-box-archive"><span class="number-top-view">2</span>
+                                            <h3 class="p-title">Tướng Trung Quốc: Một số nước cố tình làm thế giới hỗn loạn
+                                            </h3>
+                                        </div>
+                                        <div class="p-box-archive"><span class="number-top-view">2</span>
+                                            <h3 class="p-title">Tướng Trung Quốc: Một số nước cố tình làm thế giới hỗn loạn
+                                            </h3>
+                                        </div>
+                                        <div class="p-box-archive"><span class="number-top-view">2</span>
+                                            <h3 class="p-title">Tướng Trung Quốc: Một số nước cố tình làm thế giới hỗn loạn
+                                            </h3>
+                                        </div>
+                                        <div class="p-box-archive"><span class="number-top-view">2</span>
+                                            <h3 class="p-title">Tướng Trung Quốc: Một số nước cố tình làm thế giới hỗn loạn
+                                            </h3>
+                                        </div>
+                                        <div class="p-box-archive"><span class="number-top-view">2</span>
+                                            <h3 class="p-title">Tướng Trung Quốc: Một số nước cố tình làm thế giới hỗn loạn
+                                            </h3>
+                                        </div>
+                                        <div class="p-box-archive"><span class="number-top-view">2</span>
+                                            <h3 class="p-title">Tướng Trung Quốc: Một số nước cố tình làm thế giới hỗn loạn
+                                            </h3>
+                                        </div>
+                                    
                                     </div>
                                 </div>
-                                <div class="col-6 box-border box-border-right ">
+                                <!-- <div class="col-6 box-border box-border-right ">
                                     <div class="p-box-border">
                                         <li class="p-month-archive ">
                                             <span class="p-number">5</span><a href="#">Giáo sư Mỹ tìm lại mẹ ruột cho hai
@@ -86,7 +105,7 @@ if (is_active_sidebar('archive-1')): ?>
 
                                         </li>
                                     </div>
-                                </div>
+                                </div> -->
                             </div>
                         </div>
 
