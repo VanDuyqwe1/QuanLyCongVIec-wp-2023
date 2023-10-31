ebe6cff5c4e5211638bbb9a41936c6654cabf71b
Ngoc Phong <ngocphongdnp@gmail.com>
2023-10-31 21:06:16 +0700

    update 21h06

--- wp-content/themes/twentytwentyone/template-parts/archive/archive-widgets.php
+++ wp-content/themes/twentytwentyone/template-parts/archive/archive-widgets.php
@@ -43,7 +43,7 @@ if (is_active_sidebar('archive-1')): ?>
                                                     <?php echo $id ?>
                                                 </span>
                                                 <h3 class="p-title">
-                                                    <?php echo $meta_value ?>
+                                                    <?php echo $meta_value ?> <svg class="p-font-icon" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"> <path d="M448 0H64C28.7 0 0 28.7 0 64v288c0 35.3 28.7 64 64 64h96v84c0 9.8 11.2 15.5 19.1 9.7L304 416h144c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64z"/> </svg> <a href="" class="p-number-archive"><?php echo $post_parent ?></a>
                                                 </h3>
                                             </div>
                                         <?php } ?>
